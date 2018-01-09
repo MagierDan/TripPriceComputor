9002772ebe97fd2fe79a758cc6116e1720e13c96
bloumix <xavier.bloumine@gmail.com>
2018-01-09 17:34:19 +0100

    myCommit

--- exposition/src/main/java/com/magier/tripservice/exposition/Application.java
+++ exposition/src/main/java/com/magier/tripservice/exposition/Application.java
@@ -3,12 +3,11 @@ package com.magier.tripservice.exposition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.magier.tripservice", "com.magier.tripservice.domain"})
 @EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.magier.tripservice"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ infrastructure/src/main/java/com/magier/tripservice/infrastructure/travel/fees/TravelFeesMockRepositoryAdapter.java
@@ -0,0 +1,40 @@
+package com.magier.tripservice.infrastructure.travel.fees;
+
+import com.magier.tripservice.domain.TravelFeesRepositoryPort;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TravelFeesMockRepositoryAdapter implements TravelFeesRepositoryPort {
+
+    @Override
+    public Integer getTravelFeesByDestination(String destination) {
+        switch (destination) {
+            case "Paris":
+                return 250;
+            case "Lille":
+                return 0;
+            case "New-York":
+            case "Tokyo":
+            case "Beijing":
+                return 1000;
+            default:
+                throw new IllegalArgumentException("Invalid destination: " + destination);
+        }
+    }
+
+    @Override
+    public Integer getAgencyFeesByDestination(String destination) {
+        switch (destination) {
+            case "Paris":
+                return 25;
+            case "Lille":
+                return 0;
+            case "New-York":
+            case "Tokyo":
+            case "Beijing":
+                return 100;
+            default:
+                throw new IllegalArgumentException("Invalid destination: " + destination);
+        }
+    }
+}
